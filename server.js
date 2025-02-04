6f29787f1e2c629bfc69985389584d5d84add49f
Aman Rajput <amanrohilla520@gmail.com>
2025-02-04 11:50:26 +0530

    Update

---
+++ server.js
@@ -0,0 +1,14 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.get("/ping", (req, res) => {
+    res.send("pong");
+});
+
+app.listen(PORT, () => {
+    console.log(\Server is running on port ${PORT}\);
+});
