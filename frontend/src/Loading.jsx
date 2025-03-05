f181990b741a63dd2091f6730656d5ee5d925bce
AMAN AMAN <amanrohilla520@gmail.com>
2025-03-05 10:52:32 +0530

    React Start

---
+++ frontend/src/Loading.jsx
@@ -0,0 +1,10 @@
+function LandingPage() {
+    return (
+        <div className="landing-container">
+        <h1>Welcome to My ASAP EXPENSE TRACKER PROJECT</h1>
+        <p>MY NAME IS AMAN.</p>
+      </div>
+    );
+  }
+  
+  export default LandingPage;
