ec044cf6cf698dced4ebafd291d44fff8798a6e0
AMAN AMAN <amanrohilla520@gmail.com>
2025-03-05 12:21:32 +0530

    React Start

--- frontend/src/Loading.jsx
+++ frontend/src/Loading.jsx
@@ -1,10 +1,11 @@
-function LandingPage() {
-    return (
-        <div className="landing-container">
-        <h1>Welcome to My ASAP EXPENSE TRACKER PROJECT</h1>
-        <p>MY NAME IS AMAN.</p>
-      </div>
-    );
-  }
-  
-  export default LandingPage;
+const LandingPage = () => {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-600">Welcome to My ASAP EXPENSE TRACKER PROJECT</h1>
+      <p className="mt-4 text-lg text-yellow-500">MY NAME IS AMAN.</p>
+
+    </div>
+  );
+};
+
+export default LandingPage;
